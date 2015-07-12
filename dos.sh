b3cdb2f95237b96b286b744b921a31c088598844
yufengliang <pcbee912@gmail.com>
2015-07-11 17:57:43 -0700

    Complete the first dos.sh (need improvement)

--- dos.sh
+++ dos.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
-function run_dos() {
+run_dos() {
+
+  echo $job_separator 
+  echo "Job = $job"
+
+  if [ ! -d bands ]; then
+    echo "Please run a bands calculation first !"
+    return
+  fi
+
+  cd bands
+
+  if [ $BANDS_OK -eq 0 ]; then
+    echo "BANDS not completed. Go back to check it !"
+    cd ../
+    return
+  elif [ ! -d $VTSTSCRIPTS ]; then
+    echo "Please specify: VTSTSCRIPTS=/your/vtstscripts/path. "
+    cd ../
+    return
+  else
+    $VTSTSCRIPTS/split_dos.py
+  fi
 }
  
